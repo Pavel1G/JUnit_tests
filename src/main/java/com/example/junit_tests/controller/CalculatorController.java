6ab7b60dff98052c9d24ba00bc41ac85cbd4bfa5
Pavel1G <94784915+Pavel1G@users.noreply.github.com>
2022-11-23 23:28:50 +0300

    HW done

--- src/main/java/com/example/junit_tests/controller/CalculatorController.java
+++ src/main/java/com/example/junit_tests/controller/CalculatorController.java
@@ -2,6 +2,7 @@ package com.example.junit_tests.controller;
 
 import com.example.junit_tests.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,25 @@ public class CalculatorController {
     @GetMapping("/calculator")
     public String greetingCalc() {
         return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping("/calculator/plus")
+    public String additionNumbers(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+        return num1 + " + " + num2 + " = " + calculatorService.additionNumbers(num1, num2);
+    }
+
+    @GetMapping("/calculator/minus")
+    public String subtractionNumbers(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+        return num1 + " - " + num2 + " = " + calculatorService.subtractionNumbers(num1, num2);
+    }
+
+    @GetMapping("/calculator/multiply")
+    public String multiplyNumbers(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+        return num1 + " * " + num2 + " = " + calculatorService.multiplyNumbers(num1, num2);
+    }
+
+    @GetMapping("/calculator/divide")
+    public String divideNumbers(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+        return num1 + " / " + num2 + " = " + calculatorService.divideNumbers(num1, num2);
     }
 }
--- src/main/java/com/example/junit_tests/service/CalculatorService.java
+++ src/main/java/com/example/junit_tests/service/CalculatorService.java
@@ -4,4 +4,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+
+    public int additionNumbers(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+    public int subtractionNumbers(int num1, int num2) {
+        return (num1 - num2);
+    }
+
+    public int multiplyNumbers(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public float divideNumbers(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("На ноль делить нельзя!");
+        }
+        return num1 / num2;
+    }
 }
